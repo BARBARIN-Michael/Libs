c48953f79ff9aa3728e7eebff00924543939ae8e
Michael barbarin <michael.barbarin@gmail.com>
2017-02-13 12:21:30 +0100

    del function not implemented and with error

--- libft/src/ft_memchr.c
+++ libft/src/ft_memchr.c
@@ -6,26 +6,12 @@
 /*   By: mbarbari <mbarbari@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/04 14:53:55 by mbarbari          #+#    #+#             */
-/*   Updated: 2014/11/09 22:13:09 by mbarbari         ###   ########.fr       */
+/*   Updated: 2017/02/13 12:20:55 by barbare          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <string.h>
-
-void	*ft_memstrstr(const void *mem, const char *cmp, size_t size)
-{
-	size_t		i;
-
-	i = 0;
-	while (i < size)
-	{
-		if (ft_memcmp(&mem[i], cmp, ft_strlen(cmp)) == 0)
-			return ((void *)&mem[i]);
-		++i;
-	}
-	return (NULL);
-}
 
 void	*ft_memchr(const void *src, int c, size_t len)
 {
